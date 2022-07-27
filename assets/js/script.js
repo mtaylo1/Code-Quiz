0f2639fde0324e71e0eaf054805d01f4b9905559
M Taylor <107767318+mtaylo1@users.noreply.github.com>
2022-07-26 22:47:01 -0400

    adding quiz questions

---
+++ assets/js/script.js
@@ -0,0 +1,58 @@
+const quizData = [
+    {
+        Question: "What does CSS stand for?"
+        a: "Continous Spread Sheet"
+        b: "Creative Style Sheet"
+        c: "Cascading Style Sheet"
+        d: "Collaborative Style Sheet"
+    }
+
+    {
+        Question: "When was the first website made?"
+        a: "April 1st 2000"
+        b: "March 9th 1997"
+        c: "January 16th 1979"
+        d: "April 30th 1993"
+    }
+
+    {
+        Question: "What does HTML stand for?"
+        a: "Hyper Text Marketing Language"
+        b: "Hole Textfile Makeup Log"
+        c: "Herd Text Makup Log"
+        d: "Helping Text Marketing Link"
+    }
+
+    {
+        Question: "What doe APIs do"
+        a: "Outlines the specifics of software applications, telling components how they should act on an interface."
+        b: "Automatically creates Websites"
+        c: "Applies interfaces"
+        d: "Mirrors interfaces on other computers"
+    }
+
+    {
+        Question: "What is a <div> tag?"
+        a: "Divides elements in a page to the bottom"
+        b: "Used as a container for HTML elements"
+        c: "Directly Injecting Variables"
+        d: "Mirrors interfaces on other computers"
+    }
+
+
+    {
+        Question: "#ffff gives you what colour"
+        a: "White"
+        b: "Brown"
+        c: "Black"
+        d: "Fuscia"
+    }
+
+    {
+        Question: "Flexbox is an example of what"
+        a: "CSS 3 web layout model."
+        b: "A CSS element that boxes any element"
+        c: "A playable video that varies in size"
+        d: "A customizable generator that creates elements based on size"
+    }
+]
